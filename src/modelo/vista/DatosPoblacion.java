7ba3f21cba410542206d7ff60acbc127d601bb52
manufuentes98 <36626964+manufuentes98@users.noreply.github.com>
2019-05-06 18:31:08 +0200

    nuevo valor(desempleados) en la vista

--- src/modelo/vista/DatosPoblacion.java
+++ src/modelo/vista/DatosPoblacion.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 
 public class DatosPoblacion extends Datos{
 	
-	public DatosPoblacion(long habitantes, long menores, long trabajadores, long jubilados, long nacimientos, long fallecimientos,
+	public DatosPoblacion(long habitantes, long menores, long trabajadores,long desempleados, long jubilados, long nacimientos, long fallecimientos,
 			long jubilaciones, long nuevosTrabajadores) {
 		super();
 		ArrayList<Long> datos = getDatos();
 		datos.add(habitantes);
 		datos.add(menores);
+		datos.add(desempleados);
 		datos.add(trabajadores);
 		datos.add(jubilados);
 		datos.add(nacimientos);
